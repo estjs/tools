1230dfc84b917e8fcd99063b6baf55ade11912e3
jiangxd <jiangxd2016@gmail.com>
2022-06-29 17:32:41 +0800

    chore: update tsconfig dom api

--- src/filterParams.ts
+++ src/filterParams.ts
@@ -1,40 +1,19 @@
 /**
- * TODO:
  * @param params
  * @returns
  */
-export function filterParams(params) {
-  const { __noTrim__, ...p } = params;
+export function filterParams(params: { [key in string]: any } & { __trim__: boolean }) {
+  const { __trim__, ...p } = params;
   const str = JSON.stringify(p);
   let res: any = {};
   res = JSON.parse(str, (key, val) => {
     if (val !== undefined && key.indexOf('_') !== 0) {
-      /*  检测是否含有全角，并转化为半角 最后调用trim方法去前后空格 */
       if (typeof val === 'string') {
-        return __noTrim__ ? val : val.trim();
+        return __trim__ ? val.trim() : val;
       }
       return val;
     }
   });
-  return filterSecLevel(res);
+  return res;
 }
 
-function filterSecLevel(p) {
-  const re = JSON.stringify(p, (key, val) => {
-    const type = Object.prototype.toString.call(val);
-    if (key && (type === '[object Object]' || type === '[object Array]')) {
-      return filterNull(val);
-    }
-    return val;
-  });
-  return JSON.parse(re);
-}
-
-function filterNull(p) {
-  const str = JSON.stringify(p);
-  return JSON.parse(str, (key, val) => {
-    if (val !== null) {
-      return val;
-    }
-  });
-}
