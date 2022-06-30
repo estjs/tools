00a045d0336798ff0d8652891aba9c8a21328b30
jiangxd <jiangxd2016@gmail.com>
2022-06-30 15:50:38 +0800

    chore(test): remtoLocaleString test case

--- test/date.spec.ts
+++ test/date.spec.ts
@@ -5,7 +5,6 @@ describe('dateFormater', () => {
 
     const date = 1656510155000;
     expect(dateFormater()).toBe(new Date().toLocaleString());
-    expect(dateFormater(date)).toMatchInlineSnapshot('"2022/6/29 21:42:35"');
     expect(dateFormater(date, 'YYYY-MM-DD HH:mm:ss' )).toMatchInlineSnapshot('"2022-06-29 21:42:35"');
     expect(dateFormater(date, 'YYYY/MM/DD HH:mm:ss' )).toMatchInlineSnapshot('"2022/06/29 21:42:35"');
   });
