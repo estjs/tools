e608a2b97a0dbd4fb020b59c6a8f09535acc4508
jiangxd <jiangxd2016@gmail.com>
2022-06-30 16:11:08 +0800

    chore(test): use dayjs test date test case

--- test/date.spec.ts
+++ test/date.spec.ts
@@ -1,11 +1,12 @@
+import dayjs from 'dayjs';
 import { dateFormater } from '../src';
 describe('dateFormater', () => {
 
   it('formater', ()=>{
 
     const date = 1656510155000;
-    expect(dateFormater(date, 'YYYY-MM-DD HH:mm:ss' )).toMatchInlineSnapshot('"2022-06-29 21:42:35"');
-    expect(dateFormater(date, 'YYYY/MM/DD HH:mm:ss' )).toMatchInlineSnapshot('"2022/06/29 21:42:35"');
+    expect(dateFormater(date, 'YYYY-MM-DD HH:mm:ss' )).toBe(dayjs(1656510155000).format('YYYY-MM-DD HH:mm:ss'));
+    expect(dateFormater(date, 'YYYY/MM/DD HH:mm:ss' )).toBe(dayjs(1656510155000).format('YYYY/MM/DD HH:mm:ss'));
   });
 
 });
