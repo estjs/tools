1188e2cb6f98083250be09bfb0b5a191ab2f06bf
jiangxd <jiangxd2016@gmail.com>
2022-07-04 21:59:58 +0800

    chore: update test case

--- test/remove.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ArrRemove } from '../src';
-describe('replace', () => {
-
-  it('replaceAll api', ()=>{
-    expect(ArrRemove([1, 2, 3], 2)).toStrictEqual([1, 3]);
-    expect(ArrRemove([1, 2, 3], 4)).toStrictEqual([1, 2, 3]);
-  });
-});
