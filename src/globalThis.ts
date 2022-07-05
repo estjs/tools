796f1028d0a8904f0d93e9c4e940015d18e8ee4e
jiangxd <jiangxd2016@gmail.com>
2022-07-05 22:03:21 +0800

    chore: update global this

---
+++ src/globalThis.ts
@@ -0,0 +1,23 @@
+// @see https://mathiasbynens.be/notes/globalthis#naive-polyfill
+
+(function (Object) {
+  typeof globalThis !== 'object' && (
+  // @ts-expect-error
+    this
+      ? get()
+      : (Object.defineProperty(Object.prototype, '_T_', {
+          configurable: true,
+          get
+          // @ts-expect-error
+        }), _T_)
+  );
+  function get() {
+    // eslint-disable-next-line no-var
+    // @ts-expect-error
+    const global = this || self;
+    global.globalThis = global;
+    // @ts-expect-error
+    delete Object.prototype._T_;
+  }
+}(Object));
+export default globalThis;
